24ee46685862eaede9424951b833451b845a199a
npiriou <n.piriou@yahoo.fr>
2021-03-11 11:00:13 +0100

    get query concept

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/slider', routes.slider)
 
 app.listen(4242, () => console.log('Express server is running'))
+
+app.get('/concepts/', (req, res) => {
+  connection.query('SELECT * from concept', (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Error retrieving data')
+    } else {
+      res.status(200).json(results)
+    }
+  })
+})
