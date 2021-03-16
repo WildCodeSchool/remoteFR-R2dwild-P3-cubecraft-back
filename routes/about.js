af6d50829daa568adc89ca9192cd96f7bcf90d62
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-03-16 12:26:19 +0100

    add route post about table profile

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const connection = require('../config')
+const { check, validationResult } = require('express-validator')
 
 const router = express.Router()
 
+const userValidationMiddlewares = [
+  // email must be valid
+  check('email').isEmail(),
+  // password must be at least 8 chars long
+  check('password').isLength({ min: 8 }),
+  // let's assume a name should be 2 chars long
+  check('name').isLength({ min: 2 })
+]
+
 router.get('/', (request, res) => {
-  connection.query('SELECT * from profile', (err, results) => {
+  connection.query('SELECT * from Profile', (err, results) => {
     if (err) {
       res.status(500).send(`Error retrieving data`)
     } else {
@@ -13,42 +23,36 @@ router.get('/', (request, res) => {
   })
 })
 
-router.post('/:id', (req, res) => {
-  const { Firstname, Lastname, Jobname } = req.body
-  connection.query(
-    'INSERT INTO profile (Firstname, Lastname, Jobname) VALUES(?, ?, ?)',
-    [Firstname, Lastname, Jobname],
-    (err, results) => {
+router.post('/', (req, res) => {
+  connection.query('INSERT INTO profile SET ?', req.body, (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Error saving a user')
+    } else {
+      res.status(200).send('Successfully saved')
+    }
+  })
+})
+
+router.put('/:id', userValidationMiddlewares, (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+  const idUser = req.params.id
+  const newUser = ({ Firstname, Lastname, Jobname } = req.body)
+
+  return connection.query(
+    'UPDATE users SET ? WHERE id = ?',
+    [newUser, idUser],
+    err => {
       if (err) {
-        console.log(err)
-        res.status(500).send('Error saving a user')
+        res.status(500).send('Error updating a user')
       } else {
-        res.status(200).send('Successfully saved')
+        res.status(200).send('User updated successfully 🎉')
       }
     }
   )
 })
-
-// app.put("apropos/:id", (req, res) => {
-//   // We get the ID from the url:
-//   const idUser = req.params.id;
-
-//   // We get the data from the req.body
-//   const {Firstname, Lastname, Jobname} = req.body;
-
-//   // We send a UPDATE query to the DB
-//   connection.query(
-//     "UPDATE users SET ? WHERE id = ?",
-//     [newUser, idUser],
-//     (err, results) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(500).send("Error updating a user");
-//       } else {
-//         res.status(200).send("User updated successfully 🎉");
-//       }
-//     }
-//   );
-// });
 
 module.exports = router
