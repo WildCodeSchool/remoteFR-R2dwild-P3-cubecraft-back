f0c55a2502bf246b8a0bf5dc20777baf25125a82
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-03-30 15:11:14 +0200

    r

--- routes/upload.js
+++ routes/upload.js
@@ -1,31 +1,52 @@
-const express = require ('express')
+// const express = require('express')
+// const router = express.Router()
+// const multer = require('multer')
+
+// const upload = multer({
+//   dest: '../remoteFR-R2dwild-P3-cubecraft-front/public/images'
+// })
+// const fs = require('fs')
+// const { promisify } = require('util')
+// const pipeline = promisify(require('stream').pipeline)
+
+// router.post('/', upload.single('file'), function (req, res, next) {
+//   const { file } = req
+//   console.log(file)
+//   // pipeline(
+//   //   file.stream,
+//   //   fs.createWriteStream(`${__dirname}/remoteFR-R2dwild-P3-cubecraft-front/public/images/${file}`)
+//   // )
+//   // if (file.detectFileExtension != '.jpg' || file.detectFileExtension != '.png')
+//   //   next(new Error('Invalid file type'))
+//   // let storage = multer.diskStorage({
+//   //   destination: function (req, file, cb) {
+//   //     cb(null, '../remoteFR-R2dwild-P3-cubecraft-front/public/images')
+//   //   },
+//   //   filename: function (req, file, cb) {
+//   //     cb(null, file.originalname + '-' + Date.now())
+//   //   }
+//   //   // const filename = name + Math.floor(Math.random() * 1000) + file.detectedFileExtension
+//   // })
+//   // var upload = multer({ storage: storage })
+// })
+
+// module.exports = router
+
+const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 
-const upload = multer ({ dest: '../images' })
-const fs = require("fs")
-const  {promisify} = require ('util')
-const pipeline =  promisify(require("stream").pipeline)
+const upload = multer({
+  dest: '../remoteFR-R2dwild-P3-cubecraft-front/public/images'
+})
+const fs = require('fs')
+const { promisify } = require('util')
+const pipeline = promisify(require('stream').pipeline)
 
+router.post('/', upload.single('file'), function (req, res, next) {
+  const { file } = req
+  console.log(file)
 
-router.post('/', upload.single("file") ,async function (req, res, next) {
-    // console.log(req.file)
-    const {file} = req
-    console.log(file)
-    await pipeline(file.stream, fs.createWriteStream(`${__dirname}/images/${file}`))
-    if (file.detectFileExtension != ".jpg" || file.detectFileExtension != ".png" ) next(new Error("Invalid file type"))
-    var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, '../images')
-        },
-        filename: function (req, file, cb) {
-          cb(null, file.fieldname + '-' + Date.now())
-        }
-      })
-       
-      var upload = multer({ storage: storage })
-  
-    // const fileName= name + Math.floor(Math.random() * 1000) + file.detectFileExtension
 })
 
-module.exports = router;
+module.exports = router
