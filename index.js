19b8ca34ecd4307d2b54f6a43d383a912206b2b2
INGRID <ingridpruvost966@gmail.com>
2021-04-08 10:49:13 +0200

    pull

--- index.js
+++ index.js
@@ -13,5 +13,4 @@ app.use('/about', routes.about)
 app.use('/news', routes.news)
 app.use('/concept', routes.concept)
 app.use('/particularPro', routes.particularPro)
-
 app.listen(4242, () => console.log('Express server is running'))
